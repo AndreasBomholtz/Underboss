fb4ce5c769163a7995125a12044d61bf0f60c598
Andreas Bomholtz <andreas@seluxit.com>
2015-05-21 09:17:08 +0200

    Bump.

--- Underboss.user.js
+++ Underboss.user.js
@@ -6,7 +6,7 @@
 // @include     https://www.kabam.com/games/the-godfather/play*
 // @include     https://www.kabam.com/*/games/the-godfather/play*
 // @include     https://*godfather.*.com/platforms/kabam/game*
-// @version     2.0.0
+// @version     2.0.1
 // @grant       GM_addStyle
 // @grant       GM_getResourceText
 // @updateURL   https://raw.githubusercontent.com/AndreasBomholtz/Underboss/master/Underboss.js
