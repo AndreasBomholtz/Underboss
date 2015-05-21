d9591c1d1c53dcfd6b805c08c940dcd94001f257
Andreas Bomholtz <andreas@seluxit.com>
2015-05-21 08:40:59 +0200

    Split the bot into more sub scripts.

--- Underboss.user.js
+++ Underboss.user.js
@@ -14,18 +14,26 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
 // @require     http://underscorejs.org/underscore-min.js
 // @require     https://raw.githubusercontent.com/millermedeiros/js-signals/master/dist/signals.min.js
-// @resource    underbossCSS underboss.css
+// @resource    underbossCSS res/underboss.css
 // @require     res/buildings.js
 // @require     res/research.js
 // @require     res/prizes.js
 // @require     res/attack_units.js
 // @require     res/defense_units.js
-// @require     frame.js
-// @require     inject.js
-// @require     collect.js
-// @require     build.js
-// @require     research.js
-// @require     bot.js
+// @require     util/frame.js
+// @require     util/inject.js
+// @require     bot/collect.js
+// @require     bot/build.js
+// @require     bot/research.js
+// @require     bot/attack.js
+// @require     bot/bailout.js
+// @require     bot/bonds.js
+// @require     bot/gui.js
+// @require     bot/item.js
+// @require     bot/prize.js
+// @require     bot/report.js
+// @require     bot/train.js
+// @require     bot/bot.js
 // ==/UserScript==
 
 
@@ -34,8 +42,7 @@ var css = GM_getResourceText("underbossCSS");
 GM_addStyle (css);
 
 //Insert into the right iframe
-var str = "" + window.location.href;
-if(str.indexOf("platforms/kabam/game") != -1) {
+if(window.location.href.indexOf("platforms/kabam/game") != -1) {
     console.debug("Injecting Godfather Bot script");
 
 	// Inject scripts
@@ -51,13 +58,3 @@ if(str.indexOf("platforms/kabam/game") != -1) {
 	// Inject main script
 	injectFunction(main);
 }
-
-/*window.onerror = function(msg, url, line) {
-  console.debug(msg + " on line "+line);
-  if(line !== 0) {
-  var info = document.getElementById('debug_info');
-  if(info) {
-  info.innerHTML = msg + " on line "+line+"\n" + info.innerHTML;
-  }
-  }
-  };*/
