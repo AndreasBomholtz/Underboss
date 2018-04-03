2251ccc4e39a324e75eaa7402b5c1752ef8dcfcd
Andreas Bomholtz <andreas@seluxit.com>
2018-04-03 14:23:31 +0200

    Added new release

--- Underboss.meta.js
+++ Underboss.meta.js
@@ -5,7 +5,7 @@
 // @description A bot for the game The Godfather from kabam.com
 // @include     https://apps.facebook.com/play_godfather*
 // @include     https://*godfather.*.com/platforms/facebook/game*
-// @version     2.2.6
+// @version     2.2.7
 // @grant       GM_addStyle
 // @grant       GM_xmlhttpRequest
 // @downloadURL https://raw.githubusercontent.com/AndreasBomholtz/Underboss/master/Underboss.user.js
